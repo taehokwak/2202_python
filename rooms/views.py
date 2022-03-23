2540010254a4a8c724b78e991b5d1fecf2d1d667
teho <avecbelleet@gmail.com>
2022-03-23 21:58:26 +0900

    #11.8 done

--- rooms/views.py
+++ rooms/views.py
@@ -1,23 +1,13 @@
-from math import ceil
-from django.shortcuts import render
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = int(request.GET.get("page", 1))
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-    return render(
-        request,
-        "rooms/home.html",
-        context={
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count),
-        },
-    )
+class HomeView(ListView):
+
+    """HomeView Definition"""
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
